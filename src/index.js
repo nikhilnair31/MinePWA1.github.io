e4da2f4e4e707042721ba6a5a1809b6d68267c09
Nikhil Nair <niknair3189@gmail.com>
2021-01-05 13:20:59 +0530

    Added simple Online/Offline text on load

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ if('serviceWorker' in navigator){
         console.log(error);
     });
 }
+
+//If PWA online then sync local and cloud db and if offline use available local db only
+//Once connection is restablished refresh/recopy db from cloud to local?
+//Check if connection exists at regular intervals instead of only on load
+document.getElementById("conn_status").textContent = (window.navigator.onLine) ? "Online" : "Offline";
